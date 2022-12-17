81c1278e8468aa81aabfb9a467597e9cd1a8da62
VladimirSherban <Vovan_Sh_94@mail.ru>
2022-12-17 17:04:58 +0300

    Corrected homework done!

--- src/com/Skypro/Main.java
+++ src/com/Skypro/Main.java
@@ -6,8 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         checkLeapYear(2022);
-        checkDeviceTypeAndYear('1', 2019);
-        howDeliveryTime(26);
+        checkDeviceTypeAndYear(1, 2019);
+
+        int day = calculateDeliveryTime(80);
+        if (day == 1) {
+            System.out.println("Доставка " + day + " день");
+        } else if (day == 2) {
+            System.out.println("Доставка " + day + " дня");
+        } else if (day == 3) {
+            System.out.println("Доставка " + day + " дня");
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 
 
@@ -15,38 +25,38 @@ public class Main {
         boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
         if (isLeapYear) {
             System.out.println(year + " год является високосным");
-        } else
+        } else {
             System.out.println(year + " год не является високосным");
+        }
     }
 
-    public static void checkDeviceTypeAndYear(char clientOs, int clientDeviceYear) {
+    public static void checkDeviceTypeAndYear(int clientOs, int clientDeviceYear) {
         System.out.println("\nЗадача_2");
         int currentYear = LocalDate.now().getYear();
-        if (clientOs == '0' && clientDeviceYear < currentYear) {
+        if (clientOs == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOs == '0') {
+        } else if (clientOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOs == '1' && clientDeviceYear < currentYear) {
+        } else if (clientOs == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
 
-    public static void howDeliveryTime(int deliveryDistance) {
+    public static int calculateDeliveryTime(int deliveryDistance) {
         System.out.println("\nЗадача_5");
-        int day = 1;
+        int day;
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: " + day + " день доставки");
+            day = 1;
         } else if (deliveryDistance <= 60) {
-            day += 1;
-            System.out.println("Потребуется дней: " + day + " дня доставки");
+            day = 2;
         } else if (deliveryDistance <= 100) {
-            day += 2;
-            System.out.println("Потребуется дней: " + day + " дня доставки");
+            day = 3;
         } else {
-            System.out.println("Доставки нет");
+            day = 4;
         }
+        return day;
     }
 }
 
